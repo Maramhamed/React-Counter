ba3f11a7828331368793805c04e0ce294680ded1
maramhamed <Maramhamed83@gmail.com>
2023-08-31 21:10:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(0);
+  console.log(num);
+  // function Increase() {
+  //   return setNum((n) => n + 1);
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="row justify-content-center align-items-center">
+        <div className="col-2 text-center">
+          <button
+            className="btn btn-success"
+            onClick={() => setNum((n) => n + 1)}
+          >
+            Increase
+          </button>
+        </div>
+        <div className="col-2 text-center">
+          <h3 className="mt-3 mb-0">{num}</h3>
+        </div>
+        <div className="col-2 text-center">
+          <button
+            className="btn btn-success"
+            onClick={() => setNum((n) => n - 1)}
+          >
+            Decrease
+          </button>
+        </div>
+      </div>
+      <div className="row justify-content-center align-items-center mt-5">
+        <div className="col-2 text-center">
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={() => setNum(0)}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
